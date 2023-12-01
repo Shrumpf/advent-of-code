b82e8a111b7be70f5b1fa10a276b9fed3e36b058
Shrumpf <flori.voss@gmx.net>
2023-12-01 22:32:07 +0100

    fix(download-input): default to current date and use session

--- scripts/download-input.js
+++ scripts/download-input.js
@@ -1,23 +1,23 @@
+#!/usr/bin/env node
+
+import 'dotenv/config'
+import path from "node:path";
 import minimist from "minimist";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { existsSync } from "node:fs";
 
 const { AOC_USER_AGENT, AOC_SESSION_COOKIE } = process.env;
 
 let { year, day } = minimist(process.argv.slice(2), {
     string: ["year", "day"],
     default: {
-        "year": "2020",  // new Date().getFullYear().toString()
-        "day": "01", // new Date().getDay().toString().padStart(2, "0")
+        "year": new Date().getFullYear().toString(),
+        "day": new Date().getDate().toString().padStart(2, "0")
     }
 });
 
 day = day.padStart(2, "0");
 
-console.log(process.argv.slice(3));
-
-
-import { existsSync } from "node:fs";
-import { mkdir, readFile, writeFile } from "node:fs/promises";
-import path from "node:path";
 
 /**
  * 
@@ -26,12 +26,21 @@ import path from "node:path";
  * @returns 
  */
 async function getInput(year, day) {
-    const res = await fetch(`https://adventofcode.com/${year}/day/${parseInt(day)}/input`, {
+    const url = new URL(`https://adventofcode.com/${year}/day/${parseInt(day)}/input`);
+
+    if (!AOC_SESSION_COOKIE && !AOC_USER_AGENT) {
+        throw new Error("No Session or User-Agent");
+    }
+
+    const res = await fetch(url.href, {
         method: "GET",
+        credentials: "include",
         headers: {
             "Cookie": `session=${AOC_SESSION_COOKIE}`,
             "User-Agent": AOC_USER_AGENT,
-            "Accept": "text/plain",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": "1",
         }
     });
 
